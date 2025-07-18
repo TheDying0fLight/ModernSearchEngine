4e789cefb3abac18af42a90b453aa966f03de8ca
Jan-Malte Giannikos <janmaltegiannikos@gmail.com>
2025-07-18 13:50:43 +0200

    new sentence wise retriever and pipeline code in a python file for ease of execution

--- project/retriever/model.py
+++ project/retriever/model.py
@@ -4,6 +4,9 @@ import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModel
 from safetensors.torch import load_file
 from transformers.models.clip.modeling_clip import clip_loss
+import nltk
+import math
+import numpy as np
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -48,3 +51,143 @@ class RetrieverModel(nn.Module):
         state_dict = load_file(path, device)
         self.load_state_dict(state_dict, strict=False)
         return self
+    
+class ColSentenceModel(nn.Module):
+    def __init__(self, model_name="prajjwal1/bert-mini", embed_size=128, loss_type="siglip"):
+        super().__init__()
+        self.model_name = model_name
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.bert_model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+        self.token_mapper = nn.Linear(self.bert_model.pooler.dense.out_features, embed_size)
+        self.loss_type=loss_type
+        nltk.download('punkt_tab') # install the sentence level tokenizer
+        self.to(device)
+    """
+    def loss_function(self, predictions, gt_labels):
+        true_labels_mask = gt_labels.bool()
+        false_labels_mask = torch.logical_not(true_labels_mask)
+        return - torch.sum(torch.log(predictions[true_labels_mask])) - torch.sum(torch.log(torch.ones_like(predictions[false_labels_mask])-predictions[false_labels_mask]))
+    """
+    # Intended tensor shapes: 
+    # doc_tokens: (batch_size, doc tokens (num sentences) -> may have padding, embedding size)
+    # query_tokens: (batch_size, embedding size, query tokens (num sentences) -> may have padding)
+    def max_sim(self, doc_tokens, query_tokens, sentence_wise, full_mat=True):
+        if full_mat:
+            desired_query_shape = (query_tokens.shape[0], doc_tokens.shape[0], query_tokens.shape[1], query_tokens.shape[2])
+            desired_doc_shape = (query_tokens.shape[0], doc_tokens.shape[0], doc_tokens.shape[1], doc_tokens.shape[2])
+            query_tokens = query_tokens.unsqueeze(1).expand(desired_query_shape).flatten(0,1)
+            doc_tokens = doc_tokens.unsqueeze(0).expand(desired_doc_shape).flatten(0,1)
+            if sentence_wise:
+                return torch.sum(torch.bmm(doc_tokens, query_tokens), dim=2).reshape((desired_query_shape[0], desired_query_shape[1], desired_doc_shape[2]))
+            else:
+                return torch.sum(torch.max(torch.bmm(doc_tokens, query_tokens), dim=1, keepdim=True)[0], dim=2).reshape((desired_query_shape[0], desired_query_shape[1])) # shape after bmm (batch size, #doc_tokens, #query_tokens)
+        else:
+            if sentence_wise:
+                return torch.sum(torch.bmm(doc_tokens, query_tokens), dim=2)
+            else:
+                return torch.sum(torch.max(torch.bmm(doc_tokens, query_tokens), dim=1, keepdim=True)[0], dim=2) # shape after bmm (batch size, #doc_tokens, #query_tokens)
+
+    def custom_embed(self, sentences, idx_map): # sentences structure: (batch x sentences) embeddings
+        tokens = self.tokenizer(sentences, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
+        embeddings = self.bert_model(**tokens).pooler_output
+        final_embeddings = torch.nn.functional.normalize(self.token_mapper(embeddings),dim=1)
+        return torch.nn.utils.rnn.pad_sequence([final_embeddings[start:end] for start, end in idx_map], batch_first=True)
+    
+    def extract_sentences(self, texts):
+        sentence_lists = []
+        idx_map = []
+        low_idx = 0
+        for text in texts:
+            sentences = nltk.tokenize.sent_tokenize(text, language='english')
+            sentence_lists.extend(sentences)
+            idx_map.append((low_idx, low_idx+len(sentences)))
+            low_idx = low_idx+len(sentences)
+        return sentence_lists, idx_map
+    
+    def forward(self, query, answer, return_loss=True, sentence_wise=False):
+        query_sentences, query_idx_map = self.extract_sentences(query)
+        document_sentences, document_idx_map = self.extract_sentences(answer)
+        query_embeddings = self.custom_embed(query_sentences, query_idx_map)
+        query_embeddings = torch.transpose(query_embeddings, 1, 2)
+        document_embeddings = self.custom_embed(document_sentences, document_idx_map)
+        logits = self.max_sim(document_embeddings, query_embeddings, sentence_wise)
+        if return_loss:
+            match self.loss_type:
+                case "siglip": loss = self.siglip_loss(logits)
+                case "clip": loss = clip_loss(logits)
+        else: loss = None
+        return {"loss": loss, "logits": logits}
+    
+    def siglip_loss(self, logits):
+        sim = logits + self.bias
+        eye = torch.eye(sim.size(0), device=sim.device)
+        y = -torch.ones_like(sim) + 2 * eye
+        loglik = F.logsigmoid(y * sim)
+        nll = -torch.sum(loglik, dim=-1)
+        loss = nll.mean()
+        return loss
+
+    def load(self, path):
+        state_dict = load_file(path, device)
+        self.load_state_dict(state_dict, strict=False)
+        return self
+    
+class BM25():
+    def __init__(self,b=0.75, k=1.2):
+        self.b = b
+        self.k = k
+
+    def bag_words(self, words, doc_freqs):
+        word_bag = {}
+        for word in words:
+            if word in word_bag.keys():
+                word_bag[word] += 1
+            else:
+                word_bag[word] = 1
+                if word in doc_freqs.keys():
+                    doc_freqs[word] += 1
+                else:
+                    doc_freqs[word] = 1
+        return word_bag, doc_freqs
+    
+    def bag_documents(self, documents):
+        word_bags = []
+        doc_lengths = []
+        doc_freqs = {}
+        for document in documents:
+            words = nltk.tokenize.word_tokenize(document)
+            doc_lengths.append(len(words))
+            word_bag, doc_freqs = self.bag_words(words, doc_freqs)
+            word_bags.append(word_bag)
+        idfs = self.calc_idf(doc_freqs, len(doc_lengths))
+        if doc_lengths > 0:
+            avgdl = sum(doc_lengths)/len(doc_lengths)
+        else:
+            avgdl = 0
+        return word_bags, idfs, avgdl
+    
+    def calc_idf(self, doc_freqs, num_docs):
+        idfs = {}
+        for term in doc_freqs.keys():
+            idfs[term] = math.log((num_docs - doc_freqs[term] + 0.5)/(doc_freqs[term] + 0.5) + 1)
+        return idfs
+
+    def preprocess(self, documents):
+        self.documents = documents # order of documents determines the implicit indexing
+        word_bags, idfs, avgdl = self.bag_documents(documents)
+        self.word_bags = word_bags
+        self.idfs = idfs
+        self.avgdl = avgdl
+
+    def calculate_rels(self, query):
+        query_terms = nltk.tokenize.word_tokenize(query)
+        relevance_list = []
+        for doc_idx in range(len(self.word_bags)):
+            relevance_list.appen(self.get_relevance(query_terms, doc_idx))
+        return np.array(relevance_list)
+
+    def get_relevance(self, query_terms, doc_idx):
+        score = 0
+        for query_term in query_terms:
+            score += self.idfs[query_term] * (self.word_bags[doc_idx][query_term]*(self.k + 1))/(self.word_bags[doc_idx][query_term]+self.k*(1-self.b+self.b*len(self.word_bags)/self.avgdl))
+        return score
---
+++ project/retriever/pipeline.py
@@ -0,0 +1,82 @@
+from model import RetrieverModel, ColSentenceModel
+from datasets import load_dataset
+import os
+from transformers import Trainer, TrainingArguments
+from callbacks import NotebookProgressCallbackNoTable
+from transformers.utils.notebook import NotebookProgressCallback
+from evaluation import compute_metrics
+import numpy as np
+import wandb
+
+# loss = "siglip"
+loss = "clip"
+batch_size = 64
+# epochs = 2 * batch_size
+epochs = 1
+lr = 1e-6
+eval_batch = 256
+
+model = ColSentenceModel(loss_type=loss)
+dataset_name = "microsoft/ms_marco"
+dir_name = "v2.1"
+dataset = load_dataset(dataset_name, dir_name)
+
+lr_n = "" if lr == 1e-7 else f"lr{lr:.0E}_"
+b_n = "" if batch_size == 2 else f"b{batch_size}_"
+
+model_name = model.model_name.split("/")[-1]
+model_path = f"{loss}/{model_name}/{b_n}{lr_n}{dataset_name}{dir_name}"
+print(model_path)
+print(dataset)
+
+os.environ["WANDB_PROJECT"] = "MSE"
+os.environ["WANDB_LOG_MODEL"] = "false"
+wandb.init(entity="mse-jan-simon", name=model_path)
+
+training_args = TrainingArguments(
+    output_dir="models/" + model_path,
+    per_device_train_batch_size=batch_size,
+    num_train_epochs=epochs,
+    learning_rate=lr,
+    save_steps=1000,
+    save_total_limit=1,
+    remove_unused_columns=False,
+    bf16=True,
+    optim="adamw_bnb_8bit",
+    logging_steps=100,
+    eval_steps=200,
+    eval_strategy="steps",
+    eval_on_start=True,
+    per_device_eval_batch_size=eval_batch,
+    run_name=model_path,
+    report_to='wandb',
+    # max_steps=1000,
+)
+
+def collate_fn(batch):
+    def get_relevant(passages):
+        passage = np.array(passages["passage_text"])[np.array(passages["is_selected"], bool)]
+        return passage[0] if len(passage) > 0 else ""
+    res = {
+        "query": [ex["query"] for ex in batch],
+        # "answer": [ex["answers"][0] if ex["answers"] else "" for ex in batch],
+        "answer": [get_relevant(ex["passages"]) for ex in batch],
+    }
+    return res
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=dataset["train"],
+    eval_dataset=dataset["test"].take(10000),
+    data_collator=collate_fn,
+    compute_metrics=compute_metrics
+)
+
+trainer.remove_callback(NotebookProgressCallback)
+trainer.add_callback(NotebookProgressCallbackNoTable)
+
+# trainer.evaluate()
+trainer.train()
+# try: trainer.train(resume_from_checkpoint=True)
+# except: trainer.train(resume_from_checkpoint=False)
